831382f842ea31b65bcd954a28598177179e3bbe
catfish <bogdan_redko@ukr.net>
2018-10-09 16:00:30 +0300

    hello world2

--- recources.tf
+++ recources.tf
@@ -1,8 +1,29 @@
 resource "aws_vpc" "environment-example-two" {
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
   tags {
    Name = "Terraform-aws-vpc-example-two"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "eu-central-1a"
+  tags {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_security_group" "subnetsequrity" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  }
+  tags {
+    Name = "terraform test SD"
   }
 }
